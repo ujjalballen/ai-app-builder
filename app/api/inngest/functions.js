68d8ed3630d1c5f9d50e0187e6566d3cc9eac47c
Ujjal B. Allen <ujjal.vision02@gmail.com>
2026-01-21 22:45:23 +0600

    e2b sandboxes

--- app/api/inngest/functions.js
+++ app/api/inngest/functions.js
@@ -1,11 +1,17 @@
 import { inngest } from "@/app/inngest/client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from '@e2b/code-interpreter'
 
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "agent/hello" },
   async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sendbox-id", async () => {
+      const sandBox = await Sandbox.create("desktop");
+      return sandBox.sandboxId;
+    });
 
     const helloAgent = createAgent({
       name: "Hello Agent",
@@ -15,6 +21,14 @@ export const helloWorld = inngest.createFunction(
     });
 
     const { output } = await helloAgent.run("Say hello to the user");
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await Sandbox.connect(sandboxId);
+      const host = sandbox.getHost();
+
+      console.log(`General sandbox host URL: ${host}`);
+      return host;
+    })
 
     return {
       message: output[0].content
