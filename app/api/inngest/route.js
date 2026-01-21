97c256280f2c95f2b7b034551cb34f356506384c
Ujjal B. Allen <ujjal.vision02@gmail.com>
2026-01-21 12:21:08 +0600

    implement the background jobs with inngest

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import { helloWorld } from "./functions";
+import { inngest } from "@/app/inngest/client";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        /* your functions will be passed here later! */
+        helloWorld
+    ],
+});
